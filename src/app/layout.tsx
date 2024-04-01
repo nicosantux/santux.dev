999630ba53214a8b95d43efefcb387b5b719759b
Nicolás Santuccio <nico.santuccio@gmail.com>
2024-04-01 17:10:47 -0300

    style: implement local fonts and theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,24 @@
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
 import { type ReactNode } from 'react'
 
 import { type Metadata } from 'next'
 
+import { ThemeProvider } from '@/providers/theme-provider'
+
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const cabinet = localFont({
+  src: './assets/fonts/cabinet-grotesk-variable.woff2',
+  display: 'swap',
+  variable: '--font-cabinet',
+})
+
+const satoshi = localFont({
+  src: './assets/fonts/satoshi-variable.woff2',
+  display: 'optional',
+  variable: '--font-satoshi',
+})
 
 export const metadata: Metadata = {
   title: 'santux.dev',
@@ -19,8 +31,16 @@ interface LayoutProps {
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang='es'>
-      <body className={`${inter.className} bg-zinc-900 text-zinc-100`}>{children}</body>
+    <html lang='es' suppressHydrationWarning>
+      <body
+        className={`${cabinet.variable} ${satoshi.variable} grid min-h-dvh grid-rows-[1fr_auto] bg-background font-sans text-foreground antialiased [text-rendering:optimizeLegibility] selection:bg-accent selection:text-background`}
+      >
+        <ThemeProvider attribute='class' defaultTheme='dark' disableTransitionOnChange>
+          <main className='mx-auto w-[min(70ch,100%-2.5rem)] overflow-x-hidden px-1 py-20'>
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className='grid min-h-dvh place-items-center'>
-      <h1 className='text-2xl font-bold'>santux.dev</h1>
-    </main>
-  )
+  return <h1 className='text-center text-4xl font-bold'>santux.dev</h1>
 }
