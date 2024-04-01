3b842c8371071c7cc522c7b411db661368a0a4ef
Nicolás Santuccio <nico.santuccio@gmail.com>
2024-04-01 17:08:20 -0300

    style: add css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss'
 
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 const config: Config = {
   darkMode: 'class',
   content: [
@@ -8,8 +10,37 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: 'hsl(var(--accent))',
+        background: 'hsl(var(--background))',
+        border: 'hsl(var(--border))',
+        code: {
+          DEFAULT: 'hsl(var(--code))',
+          highlight: 'hsl(var(--code-highlight))',
+        },
+        dock: {
+          DEFAULT: 'hsl(var(--dock))',
+          foreground: 'hsl(var(--dock-foreground))',
+          icon: 'hsl(var(--dock-icon))',
+          border: 'hsl(var(--dock-border))',
+        },
+        foreground: 'hsl(var(--foreground))',
+      },
+      fontFamily: {
+        heading: ['var(--font-cabinet)', ...defaultTheme.fontFamily.sans],
+        sans: ['var(--font-satoshi)', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        h1: 'var(--heading-1)',
+        h2: 'var(--heading-2)',
+        h3: 'var(--heading-3)',
+        h4: 'var(--heading-4)',
+      },
+    },
   },
   plugins: [],
+  future: { hoverOnlyWhenSupported: true },
 }
+
 export default config
