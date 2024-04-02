2249414d3440e9bd19c3fe8e458b1786c0df1f16
Nicolás Santuccio <nico.santuccio@gmail.com>
2024-04-02 17:32:16 -0300

    feat: use dock in root layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Logo } from '@/icons/logo'
+
 export default function Home() {
-  return <h1 className='text-center text-4xl font-bold'>santux.dev</h1>
+  return (
+    <>
+      <h1 className='mb-16 flex justify-center'>
+        <span className='sr-only'>Santux</span>
+        <Logo className='w-20' />
+      </h1>
+      <p className='text-center text-xl font-bold'>En construcción...</p>
+    </>
+  )
 }
