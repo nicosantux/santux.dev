c53a919e239aafec3215012fe49c358cf5c1655c
Nicolás Santuccio <nico.santuccio@gmail.com>
2024-04-02 17:29:08 -0300

    feat: add empty blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className='grid h-full place-items-center'>
+      <h1 className='text-2xl font-bold'>En construcción...</h1>
+    </div>
+  )
+}
